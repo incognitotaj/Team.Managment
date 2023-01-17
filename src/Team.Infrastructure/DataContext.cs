238d9b5fab035bbbfee738e75ecbb664d2b88f4f
salman.t <salman.t>
2023-01-17 11:37:08 +0500

    Adding DbSet for Entities

---
+++ src/Team.Infrastructure/DataContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Team.Domain.Entities;
+
+namespace Team.Infrastructure
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Project> Projects { get; set; }
+    }
+}
